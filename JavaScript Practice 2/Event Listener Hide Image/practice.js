b880d1b1bae6810c576cbb25b129faa6cead38d6
Caylee Stewart <caylee.stewart@gmail.com>
2017-07-01 19:51:59 -0400

    added js for for event listener practice

--- JavaScript Practice 2/Event Listener Hide Image/practice.js
+++ JavaScript Practice 2/Event Listener Hide Image/practice.js
@@ -2,13 +2,21 @@ var simon = document.getElementById("simon");
 var bruce = document.getElementById("bruce");
 var ben = document.getElementById("ben");
 
-function showImage() {
-	if {
-		className === "hide";
-		className = ""
-	}else {
-		className = "hide"
+function showImage () {
+	var allImages = document.querySelectorAll("img");
+	
+	for (var i = 0; i < allImages.length; i++) {
+		allImages[i].className = "hide";
 	}
+	
+	var picId = this.attributes["data-img"].value;
+	var pic = document.getElementById(picId);
+	
+	if(pic.className === "hide") {
+			pic.className = "";
+			} else {
+				pic.className = "hide";
+			}
 }
 
 simon.addEventListener("click", showImage);
