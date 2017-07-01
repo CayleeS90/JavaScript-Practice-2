284ef701bf3451e34181b9fd965e02cf9534b265
Caylee Stewart <caylee.stewart@gmail.com>
2017-07-01 18:39:39 -0400

    Added practice apps source code

---
+++ JavaScript Practice 2/Event Listener Hide Image/practice.js
@@ -0,0 +1,16 @@
+var simon = document.getElementById("simon");
+var bruce = document.getElementById("bruce");
+var ben = document.getElementById("ben");
+
+function showImage() {
+	if {
+		className === "hide";
+		className = ""
+	}else {
+		className = "hide"
+	}
+}
+
+simon.addEventListener("click", showImage);
+bruce.addEventListener("click", showImage);
+ben.addEventListener("click", showImage);
