2b941920e65b812d003f9edb231bbff0455414ed
Caylee Stewart <caylee.stewart@gmail.com>
2017-07-07 01:21:28 -0400

    finished checklist that I can edit

--- Event Listener 2 checklist/index.js
+++ Event Listener 2 checklist/index.js
@@ -1,3 +1,30 @@
+// we want to access the ul with the id od "checklist"
 var checklist = document.getElementById("checklist");
-var items = checklist.querySelectorAll("li"); 
-
+// this gives us access to the child elements within the ul "checklist"
+var items = checklist.querySelectorAll("li");
+var inputs = checklist.querySelectorAll("input");
+/* this for loop will allow us to interact with all the list items and the elements within */
+for (var i = 0; i < items.length; i++ ) {
+	items[i].addEventListener("click", editItem);
+	inputs[i].addEventListener("blur", updateItem);
+	inputs[i].addEventListener("keypress", itemKeypress);
+	
+}
+// this will change the class which will then be in edit mode and add a focus box with highlighted value 
+function editItem () {
+	this.className = "edit"
+	var input = this.querySelector("input")
+	input.focus();
+	input.setSelectionRange(0, input.value.length);
+}
+//this will take the value from input and put it into span on blur
+function updateItem () {
+	this.previousElementSibling.innerHTML = this.value;
+	this.parentNode.className = "";
+}
+// this will do the same as updateItem except on the "enter" keypress
+function itemKeypress (event) {
+	if (event.which === 13) {
+		updateItem.call(this);
+	}
+}
